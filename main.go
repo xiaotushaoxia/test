77a241cebbbc375ce79fe17d668d5b21599de50b
xiaotushaoxia <34184824+xiaotushaoxia@users.noreply.github.com>
2024-05-06 20:47:14 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ func main() {
 	fmt.Println("hello1")
 	fmt.Println("hello2-2-2222")
 	fmt.Println("hello3-33333")
-	fmt.Println("hello4-1")
-	fmt.Println("hello5-444")
-	fmt.Println("hello56")
+	fmt.Println("hello4-4444")
+	fmt.Println("hello5-6666")
+	fmt.Println("hello56-55555")
 	fmt.Println("hello7")
 }
