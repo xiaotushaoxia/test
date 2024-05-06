3f9c9a5aa2a2002c3a35510afb766cd88b21b2bf
tuzhiwei <tuzhiwei@iplusbot.com>
2024-05-06 20:37:52 +0800

    update

--- main.go
+++ main.go
@@ -4,10 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello")
-<<<<<<< HEAD
-	fmt.Println("hello3")
-=======
 	fmt.Println("hello2")
->>>>>>> origin/main
-	fmt.Println("hello")
+	fmt.Println("hello3")
 }
