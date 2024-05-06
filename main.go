44e98420e20e72b7910ec564548b9f5229b7663e
tuzhiwei <tuzhiwei@iplusbot.com>
2024-05-06 20:47:52 +0800

    8

--- main.go
+++ main.go
@@ -6,8 +6,9 @@ func main() {
 	fmt.Println("hello1")
 	fmt.Println("hello2-2-2222")
 	fmt.Println("hello3-33333")
-	fmt.Println("hello4-1")
+	fmt.Println("hello4-111")
 	fmt.Println("hello5-444")
 	fmt.Println("hello56")
 	fmt.Println("hello7")
+	fmt.Println("hello8")
 }
